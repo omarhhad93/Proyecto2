639cbfc18457e08b1a2579ce56ff5d44bf14a041
omarhhad93 <82048799+omarhhad93@users.noreply.github.com>
2023-11-01 20:21:22 -0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,31 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  var login = {
+    "CorreoElectronico": email,
+    "clave": password
+  }
+
+  var requestOptions = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(login),
+    redirect: 'follow'
+  };
+
+  fetch("https://apisproyecto.onrender.com/api/login", requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      localStorage.setItem("token", data.token);
+      // window.location.href = "index.html";
+      window.location.href = "./html/CarritoProd.html";
+    })
+    .catch((error) => {
+      console.error("Error de inicio de sesión:", error);
+    });
+});
